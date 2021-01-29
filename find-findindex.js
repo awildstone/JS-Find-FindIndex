15d05339144ad0422b39a47bc75035e7db470d13
Aimee Wildstone <11568530+awildstone@users.noreply.github.com>
2021-01-28 18:04:15 -0800

    add files to github

---
+++ find-findindex.js
@@ -0,0 +1,42 @@
+/* 
+Write a function called `findUserByUsername` which accepts an array of objects, each with a key of username, and a string. The function should return the first object with the key of username that matches the string passed to the function. If the object is not found, return undefined. 
+
+const users = [
+  {username: 'mlewis'},
+  {username: 'akagen'},
+  {username: 'msmith'}
+];
+
+findUserByUsername(users, 'mlewis') // {username: 'mlewis'}
+findUserByUsername(users, 'taco') // undefined
+*/
+
+
+function findUserByUsername(usersArray, username) {
+  return usersArray.find(function(user, i, userArr) {
+    return user.username === username;
+  });
+}
+
+/*
+Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array. If the object is not found, return undefined. 
+
+const users = [
+  {username: 'mlewis'},
+  {username: 'akagen'},
+  {username: 'msmith'}
+];
+
+removeUser(users, 'akagen') // {username: 'akagen'}
+removeUser(users, 'akagen') // undefined
+*/
+
+function removeUser(usersArray, username) {
+  const index = usersArray.findIndex(function(user, i, userArr) {
+    return user.username === username;
+  });
+  //if the index doesn't exist return undefined
+  if (index === - 1) return undefined;
+  //if the index exists, remove index from array and return the first index of the array splice method
+  return usersArray.splice(index, 1)[0];
+}
